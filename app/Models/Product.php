f8534a18d14f1dc97eb2a7fc4f63dc73afa5ed21
trungnt173 <trungnt173@fpt.edu.vn>
2024-07-22 18:53:41 +0700

    Update code seeder 22/07

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $table = 'products';
+    protected $fillable = [
+        'pr.id',
+        'pr.name',
+        'pr.price',
+        'pr.quantity',
+        'pr.image',
+        'pr.category_id',
+        'pr.status',
+        'pr.created_at',
+        'pr.updated_at'
+    ];
+    public $timestamps = false;
+
 }
